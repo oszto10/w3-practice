2a425bb80e92a519770e3bdff0941c792110cd13
capali13 <mark.csaszarnagy@gmail.com>
2022-09-14 11:43:09 +0200

    variables

---
+++ frontend/script.js
@@ -0,0 +1,22 @@
+//var myVariable; - soha ne használd !!!
+
+let myVariable;
+console.log(myVariable);
+
+myVariable = "new value"
+console.log(myVariable);
+
+myVariable = 2;
+console.log(myVariable);
+
+myVariable = true;
+console.log(myVariable);
+
+let myVariable2 = "szöveg";
+console.log(myVariable2);
+
+myVariable = myVariable2;
+console.log(myVariable2);
+
+let myVariable3 = myVariable + myVariable2;
+console.log(myVariable3);
