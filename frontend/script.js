794bd5f3bd8bb1ab2570120851e44e77b20eddbf
capali13 <mark.csaszarnagy@gmail.com>
2022-09-14 11:58:01 +0200

    variables

--- frontend/script.js
+++ frontend/script.js
@@ -1,22 +1,37 @@
 //var myVariable; - soha ne használd !!!
-
 let myVariable;
-console.log(myVariable);
+//console.log(myVariable);
 
 myVariable = "new value"
-console.log(myVariable);
+//console.log(myVariable);
 
 myVariable = 2;
-console.log(myVariable);
+//console.log(myVariable);
 
 myVariable = true;
-console.log(myVariable);
+//console.log(myVariable);
 
 let myVariable2 = "szöveg";
-console.log(myVariable2);
+//console.log(myVariable2);
 
 myVariable = myVariable2;
-console.log(myVariable2);
+//console.log(myVariable2);
 
 let myVariable3 = myVariable + myVariable2;
-console.log(myVariable3);
+//console.log(myVariable3);
+
+function sumVariables(veriable1, veriable2) {
+    console.log(veriable1);
+}
+ sumVariables(myVariable, myVariable2);
+ sumVariables(8, "nyolc");
+ sumVariables("word1", "word2");
+
+
+
+
+
+
+
+
+
