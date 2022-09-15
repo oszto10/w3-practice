7dee06230eb1a19fb500652e534bd88418dbc68a
capali13 <mark.csaszarnagy@gmail.com>
2022-09-15 11:46:18 +0200

    get cats :)

--- frontend/script.js
+++ frontend/script.js
@@ -1,39 +1,44 @@
-//var myVariable; - soha ne használd !!!
-let myVariable;
-//console.log(myVariable);
+// //var myVariable; - soha ne használd !!!
+// let myVariable;
+// //console.log(myVariable);
 
-myVariable = "new value"
-//console.log(myVariable);
+// myVariable = "new value"
+// //console.log(myVariable);
 
-myVariable = 2;
-//console.log(myVariable);
+// myVariable = 2;
+// //console.log(myVariable);
 
-myVariable = true;
-//console.log(myVariable);
+// myVariable = true;
+// //console.log(myVariable);
 
-let myVariable2 = "szöveg";
-//console.log(myVariable2);
+// let myVariable2 = "szöveg";
+// //console.log(myVariable2);
 
-myVariable = myVariable2;
-//console.log(myVariable2);
+// myVariable = myVariable2;
+// //console.log(myVariable2);
 
-let myVariable3 = myVariable + myVariable2;
-//console.log(myVariable3);
+// let myVariable3 = myVariable + myVariable2;
+// //console.log(myVariable3);
 
-function sumVariables(veriable1, veriable2) {
-    console.log(veriable1 + veriable2);
-}
- sumVariables(myVariable, myVariable2);
- sumVariables(8, "nyolc");
- sumVariables("word1", "word2");
+// function sumVariables(veriable1, veriable2) {
+//     console.log(veriable1 + veriable2);
+// }
+//  sumVariables(myVariable, myVariable2);
+//  sumVariables(8, "nyolc");
+//  sumVariables("word1", "word2");
 
  function loadEvent() {
- console.log("loaded");
- let rootElement = document.getElementById("root");
- rootElement.insertAdjacentHTML("beforeend", "hello world")
+    const newCatButton = document.querySelector(".add-cat");
+    newCatButton.addEventListener("click", function() {
+    const catContainer = document.querySelector(".cats");
+    const now = new Date().getTime();
+    const newCat = `<img src="https://cataas.com/cat?random=${now}" alt="" />`;
+    catContainer.insertAdjacentHTML("beforeend", newCat);
+    })
+
  }
 
-window.addEventListener("click", loadEvent());
+window.addEventListener("load", loadEvent());
 
 
 
